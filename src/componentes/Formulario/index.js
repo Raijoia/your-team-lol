9a30070bc01ba74793899e00f44808fd79ae4bad
Raí <megac154@gmail.com>
2023-08-10 12:54:35 -0300

    error form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -29,7 +29,7 @@ const Formulario = (props) => {
   return (
     <section className='formulario'>
       <form onSubmit={aoSalvar}>
-        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <h2>Preencha os dados para criar o card dos integrantes</h2>
         <CampoTexto 
           required={true} 
           label='Nome' 
